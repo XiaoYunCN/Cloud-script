40a41dc6c0ab578801b8735f78c061bec67efba5
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-02-18 20:15:03 +0800

    更新 2024-2-9 Cloud script.lua

--- 2024-2-9 Cloud script.lua
+++ 2024-2-9 Cloud script.lua
@@ -1,6 +1,8 @@
-
-
-
+--[[
+第一眼：哇！云脚本源代码！
+第二眼：支持服务器？什么？
+第三眼：真服，云脚本加密，我被骗了
+]]
 local CoreGui = game:GetService("StarterGui")
 
 CoreGui:SetCore("SendNotification", {
@@ -37,14 +39,18 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-scrip
 elseif game.PlaceId == 6516141723 then
 
 CoreGui:SetCore("SendNotification", {
-    Title = "云",
-    Text = "Doors有问题，找小云作者",
+    Title = "云-警告⚠️",
+    Text = "群文件有云脚本Doors，不支持服务器哦",
     Duration = 5, 
 })
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/Doors%20Script.lia", true))()
 
 elseif game.PlaceId == 155615604 then
-
+CoreGui:SetCore("SendNotification", {
+    Title = "云",
+    Text = "正在支持服务器",
+    Duration = 5, 
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Xiao-Yun-UWU/main/%E7%9B%91%E6%8E%A7.lua",true))()
 elseif game.PlaceId == 10023052234 then
    CoreGui:SetCore("SendNotification", {
@@ -61,7 +67,13 @@ elseif game.PlaceId == 3956818381 then --忍者传奇
 }) loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/hope%20earnestly/Ninja%20Legend.lua", true))()
 
 elseif game.PlaceId == 1318971886 then --Break in (Stay)
-    loadstring(game:HttpGet("https://pastebin.com/raw/28Z4ExEn", true))()
+   
+CoreGui:SetCore("SendNotification", {
+    Title = "云",
+    Text = "当前Break in",
+    Duration = 5, 
+})
+ loadstring(game:HttpGet("https://pastebin.com/raw/28Z4ExEn", true))()
 
 elseif game.PlaceId == 189707 then 
 
@@ -73,7 +85,11 @@ CoreGui:SetCore("SendNotification", {
  loadstring(game:HttpGet("https://gist.githubusercontent.com/XiaoYunCN/ea20ccfa5b895def3d7e1442fc299c4f/raw/9f964d40a58cc66c296596eae10f38096fb6aa58/gistfile1.txt", true))()
 
 elseif game.PlaceId == 537413528 then
-
+CoreGui:SetCore("SendNotification", {
+    Title = "云",
+    Text = "正在支持服务器",
+    Duration = 5, 
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/4.lua", true))()
 
 elseif game.PlaceId == 3623096087 then --力量传奇
@@ -94,7 +110,11 @@ CoreGui:SetCore("SendNotification", {
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/%E5%AF%BB%E5%AE%9D%E6%A8%A1%E6%8B%9F%E5%99%A8.lua", true))()
 elseif game.PlaceId == 1701332290 then
         --服务号1701332290
-
+CoreGui:SetCore("SendNotification", {
+    Title = "云",
+    Text = "正在支持服务器",
+    Duration = 5, 
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/LLL.lua", true))()
 else --其他通用
 
@@ -110,6 +130,6 @@ end
 
 CoreGui:SetCore("SendNotification", {
     Title = "云脚本",
-    Text = "已经是反挂机",
+    Text = "已经是反挂机（20分钟防踢人）",
     Duration = 5, 
 })

76a25a3f0e445f471e17fb34f2dfa60535d9ffdd
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-02-20 17:25:53 +0800

    更新 自动支持服装器.lua

--- 自动支持服装器.lua
+++ 自动支持服装器.lua
@@ -1029,3 +1029,46 @@ CoreGui:SetCore("SendNotification", {
     Text = "已经是反挂机（20分钟防踢人）",
     Duration = 5, 
 })
+
+
+coroutine.wrap(CGSLH_fake_script)()
+local function VIEO_fake_script() 
+        local script = Instance.new('LocalScript', CNM)
+
+        local CNM=script.Parent
+        local Mini=CNM.Mini
+        local Close=CNM.Close
+        local main=CNM.Mian
+        main.Visible=false
+
+        Close.MouseButton1Click:Connect(function()
+                CNM.Parent:Destroy()
+        end)
+        main.Size=UDim2.new(1, 0,0.02, 0)
+        CNM.Active=true
+        CNM.Draggable=true
+        Mini.MouseButton1Click:Connect(function()
+                if main.Visible==false then
+                        main.Visible=true
+                        main:TweenSize(UDim2.new(1, 0,13.647, 0),"Out","Sine",0.15)
+                else
+                        main:TweenSize(UDim2.new(1, 0,0.02, 0),"Out","Sine",0.15)
+                        main.Visible=false
+                end
+        end)
+end
+coroutine.wrap(VIEO_fake_script)()
+game.Players.ChildAdded:Connect(function(player)
+    if not pcall (function()
+    SendNotification("玩家加入",""..player.Name.." 加入了游戏",5 )
+    end) then
+      print ("Error")
+    end
+    end)
+    game.Players.ChildRemoved:Connect(function(player)
+        if not pcall (function()
+        SendNotification("玩家离开",""..player.Name.." 离开了游戏",4.4 )
+        end) then
+          print ("Error")
+        end
+        end)

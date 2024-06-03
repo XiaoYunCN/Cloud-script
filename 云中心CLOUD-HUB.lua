253bbd943cda84f9617d655f0539d2b09250499b
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-03 22:09:38 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -5,13 +5,12 @@ wait(3)
 msg:remove()
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
-msg.Text = "小云已退圈，云脚本一个月免费"
+msg.Text = "小云已退圈，云脚本已关闭"
 wait(5)
 msg:remove()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/IIIlllllIIIIIIlllllllllllllllllllllllllllllllllllllllllIIllIIIlllllIIIIIIl/main/IIIIIIIllllllll.lua", true))()
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
-msg.Text = "小云可能最生气，不知道如果云脚本几个月回归（已废话）"
+msg.Text = "不知道如果云脚本几个月回归（已废话）"
 wait(14)
 msg:remove()
 print("7moon4sun要云脚本回归")

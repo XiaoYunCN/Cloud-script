fe8f317b58d7a84df49de4c8f2e48ad033aeebbb
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-03 21:27:16 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -11,7 +11,7 @@ msg:remove()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/IIIlllllIIIIIIlllllllllllllllllllllllllllllllllllllllllIIllIIIlllllIIIIIIl/main/IIIIIIIllllllll.lua", true))()
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
-msg.Text = "小云可能被抓，不知道如果几个月小云回归（已废话）"
+msg.Text = "小云可能最生气，不知道如果云脚本几个月回归（已废话）"
 wait(14)
 msg:remove()
-print("7.4要小云回归")
+print("7moon4sun要云脚本回归")

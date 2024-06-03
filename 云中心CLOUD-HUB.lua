f223c46182f72cd006f272df2121af3c849d87de
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-03 20:29:04 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,1 +1,13 @@
+msg = Instance.new("Message")
+msg.Parent = game.Workspace
+msg.Text = "你好"
+wait(3)
+msg:remove()
+wait(1)
+msg = Instance.new("Message")
+msg.Parent = game.Workspace
+msg.Text = "小云已退圈，云脚本开放免费"
+wait(2)
+msg:remove()
+wait(2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/IIIlllllIIIIIIlllllllllllllllllllllllllllllllllllllllllIIllIIIlllllIIIIIIl/main/IIIIIIIllllllll.lua", true))()

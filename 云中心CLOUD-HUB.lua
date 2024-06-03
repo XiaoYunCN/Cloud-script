454c3818ec14824bdd9839c6b7bdd61ed41260a1
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-03 23:49:02 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -3,11 +3,13 @@ msg.Parent = game.Workspace
 msg.Text = "你好"
 wait(3)
 msg:remove()
+wait(1)
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
 msg.Text = "小云已退圈，云脚本已关闭"
 wait(5)
 msg:remove()
+wait(1)
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
 msg.Text = "不知道如果云脚本几个月回归（已废话）"

630fb7d7571ad66535e2f6bf7c10cf63029512dc
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-04 20:40:07 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -5,7 +5,6 @@ msg.Text = "你好"
 wait(3)
 msg:remove()
 wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
 msg.Text = "小云已退圈"
@@ -14,7 +13,7 @@ msg:remove()
 wait(1)
 msg = Instance.new("Message")
 msg.Parent = game.Workspace
-msg.Text = "不知道如果云脚本几个月回归（已废话）"
+msg.Text = "2024.7.4小云回归!!!!!（已废话）"
 wait(14)
 msg:remove()
 print("7moon4sun要云脚本回归")

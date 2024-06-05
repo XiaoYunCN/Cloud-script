2640273b03c54ce748be9548fbc88069964f6ef7
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-05 23:01:43 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -5,17 +5,5 @@ CoreGui:SetCore("SendNotification", {
     Text = "2024年7月左右更新云脚本",
     Duration = 7, 
 })
-wait(2)
+wait(1.9)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()
-msg = Instance.new("Message")
-msg.Parent = game.Workspace
-msg.Text = "你好"
-wait(3)
-msg:remove()
-wait(1)
-msg = Instance.new("Message")
-msg.Parent = game.Workspace
-msg.Text = "2024.7.4小云回归!!!!!（已废话）"
-wait(14)
-msg:remove()
-print("7moon4sun要云脚本回归")

419b4e7d70fa862bcbb44bb1ced3ddff2b42d068
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-06 04:05:07 +0800

    更新 自动支持服装器.lua

--- 自动支持服装器.lua
+++ 自动支持服装器.lua
@@ -11,7 +11,7 @@ elseif game.PlaceId == 8888615802 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/%E6%B8%B8%E6%88%8F/%E5%BD%A9%E8%99%B9%E6%9C%8B%E5%8F%8B.lua", true))()
 elseif game.PlaceId == 3956818381 then
  loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/hope%20earnestly/Ninja%20Legend.lua", true))()
-elseif game.PlaceId == 1318971886 then --Break in (Stay) 
+elseif game.PlaceId == 1318971886 then
 loadstring(game:HttpGet("https://pastebin.com/raw/28Z4ExEn", true))()
 elseif game.PlaceId == 189707 then  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/L.lua", true))()

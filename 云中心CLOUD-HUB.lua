6330b3f4b4da828801854c7901e3ddd65682b66c
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-06 02:24:01 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -4,7 +4,7 @@ CoreGui:SetCore("SendNotification", {
     Title = "之前的被兄弟玩手机说成免费😡",
     Text = "2024年7月左右更新云脚本",
     Duration = 20, 
-})
+})--[[
 CoreGui:SetCore("SendNotification", {
     Title = "XY开发者",
     Text = "云脚本加强了防破解",
@@ -16,6 +16,6 @@ CoreGui:SetCore("SendNotification", {
     Text = "请加QQ2236618580（自动复制）",
     Duration = 15, 
 })
-setclipboard("2236618580")
+setclipboard("2236618580")]]
 wait(3)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()

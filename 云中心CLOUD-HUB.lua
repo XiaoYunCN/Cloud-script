39b56f901ad77e49814ddf1d02d8f4cf7d027ced
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-05 23:14:41 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -3,7 +3,13 @@ local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
     Title = "之前的被兄弟玩手机说成免费😡",
     Text = "2024年7月左右更新云脚本",
-    Duration = 7, 
+    Duration = 20, 
 })
 wait(1.9)
+CoreGui:SetCore("SendNotification", {
+    Title = "你要买云脚本吗6r",
+    Text = "请加QQ2236618580",
+    Duration = 15, 
+})
+wait(3)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()

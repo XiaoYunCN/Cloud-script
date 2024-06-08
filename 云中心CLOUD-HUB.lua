41256b7b88040be562803583b49b15ba7a7d5a5b
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-08 09:14:23 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,7 +1,7 @@
 local CoreGui = game:GetService("StarterGui")
 
 CoreGui:SetCore("SendNotification", {
-    Title = "我是小云更可能跑路了",
+    Title = "我是小云更可能跑路了😭",
     Text = "再见，我不玩roblox注入器",
     Duration = 10, 
 })

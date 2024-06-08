b4ccdf946d749bdf68af8770746d0e1fcddf646b
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-08 09:13:38 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,30 +1,8 @@
 local CoreGui = game:GetService("StarterGui")
 
 CoreGui:SetCore("SendNotification", {
-    Title = "之前的被兄弟玩手机说成免费",
-    Text = "不知道如果几个月小云回来",
+    Title = "我是小云更可能跑路了",
+    Text = "再见，我不玩roblox注入器",
     Duration = 10, 
 })
 
-CoreGui:SetCore("SendNotification", {
-    Title = "云脚本群被封了谁举报啊",
-    Text = "",
-    Duration = 10, 
-})
---[[
-CoreGui:SetCore("SendNotification", {
-    Title = "XY开发者",
-    Text = "云脚本加强了防破解",
-    Duration = 20, 
-})
-wait(1.9)
-CoreGui:SetCore("SendNotification", {
-    Title = "你要买云脚本吗6r",
-    Text = "请加QQ2236618580（自动复制）",
-    Duration = 15, 
-})
-setclipboard("2236618580")
-]]
-setclipboard("点击链接加入群聊：https://qm.qq.com/q/eVSGkUyzew"); 
-wait(3)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()

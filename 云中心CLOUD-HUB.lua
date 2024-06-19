7c264beb9a293185eced8a4ebeda8a259f79c093
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-20 02:03:20 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,2 +1,9 @@
-
+local CoreGui = game:GetService("StarterGui")
+wait(0.1)
+game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "霖溺脚本全是抄我的云源码😠😡",
+    Text = "战争大亨，造船寻宝，力量传奇",
+    Duration = 10, 
+})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/ja", true))()

cf63ade5490615b7d657e453325917356d48ca63
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-20 20:14:04 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -2,7 +2,7 @@ local CoreGui = game:GetService("StarterGui")
 wait(0.1)
 game:GetService("StarterGui")
 CoreGui:SetCore("SendNotification", {
-    Title = "霖溺脚本全是抄我的云源码😠😡",
+    Title = "霖溺脚本全是抄袭我的云源码😠😡",
     Text = "战争大亨，造船寻宝，以前的力量传奇，其他",
     Duration = 10, 
 })

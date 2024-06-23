96e6515780f35cce45aa1c3104b1b714d606f10e
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-23 13:33:32 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 7239319209 then
 Ohio = "俄亥俄州"
 loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\47\88\105\97\111\89\117\110\67\78\47\85\87\85\47\109\97\105\110\47\115\34\44\32\116\114\117\101\41\41\40\41\10")()
 elseif game.PlaceId == 1644006 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/云脚本", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC", true))()
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/云脚本", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC", true))()
 end

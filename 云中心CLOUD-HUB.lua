460bbc71a424865a45b7bf2d88ae287658f0b9ff
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-06-30 08:58:07 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,2 +1,19 @@
+local function Notif(title, text)
+    local notification = Instance.new("Notification")
+    now.Title = title
+    not.Text = text
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = now.Title,
+        Text = not.Text,
+        Duration = 5 
+    })
+end
+
+Notif("欢迎", "正常执行")
+
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC.lua", true))()
+else
+warn("加载错误")
+Notif("错误", "加载错误")

286caea775551251ce906ff9afce048360cd4adc
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-06 19:38:03 +0800

    更新 自动支持服装器.lua

--- 自动支持服装器.lua
+++ 自动支持服装器.lua
@@ -1,3 +1,47 @@
+
+--小云自己做GUI，你喜欢偷GUI是吧？🤣🤣
+function L()
+local a = Instance.new("ScreenGui")
+local b = Instance.new("Frame")
+local c = Instance.new("TextLabel")
+local d = Instance.new("TextButton")
+local e = Instance.new("TextButton")
+local f = Instance.new("TextButton")
+a.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+b.Size = UDim2.new(0.3, 0, 0.3, 0)
+b.Position = UDim2.new(0.35, 0, 0.35, 0)
+b.BackgroundColor3 = Color3.fromRGB(240, 240, 240)
+b.BorderSizePixel = 2
+b.Parent = a
+c.Text = "云脚本加载器"
+c.Size = UDim2.new(1, 0, 0.2, 0)
+c.Position = UDim2.new(0, 0, 0, 0)
+c.TextSize = 18
+c.TextColor3 = Color3.fromRGB(0, 0, 0)
+c.Parent = b
+d.Text = "加载旧版本 V1.3.0"
+d.Size = UDim2.new(0.8, 0, 0.15, 0)
+d.Position = UDim2.new(0.1, 0, 0.3, 0)
+d.Parent = b
+e.Text = "加载最新版本（限免）"
+e.Size = UDim2.new(0.8, 0, 0.15, 0)
+e.Position = UDim2.new(0.1, 0, 0.5, 0)
+e.Parent = b
+f.Text = "关闭"
+f.Size = UDim2.new(0.8, 0, 0.1, 0)
+f.Position = UDim2.new(0.1, 0, 0.8, 0)
+f.Parent = b
+d.MouseButton1Click:Connect(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/6666666666666666666666666.lua", true))()
+end)
+e.MouseButton1Click:Connect(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/%E4%BA%91%E8%84%9A%E6%9C%AC/Universal-Beta%20Version.lua", true))()
+end)
+f.MouseButton1Click:Connect(
+function()
+a:Destroy()
+end)
+end
 local CoreGui = game:GetService("StarterGui");CoreGui:SetCore("SendNotification",{Title = "等等！",Text = "正在加载",Duration = 3,})
 if game.PlaceId == 3101667897 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CloudX-ScriptsWane/ScriptsDache/main/7.lua"))()
@@ -28,5 +72,5 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-scrip
 elseif game.PlaceId == 2413927524 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/The%20Rake.lua", true))()
 else
-loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/Cloud-script/main/6666666666666666666666666.lua", true))()
+L()
 end

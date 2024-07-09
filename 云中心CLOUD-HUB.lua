49abc7d6b74f05378e60eb675c76c422d8cf86d9
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-09 17:24:55 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,1 +1,6 @@
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification",
+{Title = "云脚本提示",
+Text = "7月16日中午停止限免",
+Duration = 11,})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC.lua", true))()

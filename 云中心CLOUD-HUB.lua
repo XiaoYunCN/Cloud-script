1aeb438fe37a49390557a0a834d4b4da70d36682
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-12 19:46:48 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -9,3 +9,10 @@ CoreGui:SetCore("SendNotification",
 Text = "不支持Fluxus，该更换注入器",
 Duration = 11,})
 loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC.lua", true))()
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification",
+{Title = "云脚本提示",
+Text = "小云号将在三天后解封",
+Duration = 15,})
+print("QQ号3065560594")
+print("QQ号168777108")

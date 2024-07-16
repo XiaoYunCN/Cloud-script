c5e28cbb576874dd8fab917399a3ddd7996c2b92
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-07-16 12:57:13 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,18 +1,1 @@
-local CoreGui = game:GetService("StarterGui")
-CoreGui:SetCore("SendNotification",
-{Title = "云脚本提示",
-Text = "7月16日中午停止限免",
-Duration = 11,})
-local CoreGui = game:GetService("StarterGui")
-CoreGui:SetCore("SendNotification",
-{Title = "云脚本提示",
-Text = "不支持Fluxus，该更换注入器",
-Duration = 11,})
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IlIIIIIIIIIIIIIIIIllll/Roblox/main/%E4%BA%91%E8%84%9A%E6%9C%AC.lua", true))()
-local CoreGui = game:GetService("StarterGui")
-CoreGui:SetCore("SendNotification",
-{Title = "云脚本提示",
-Text = "小云号将在一天后解封",
-Duration = 15,})
-print("QQ号3065560594")
-print("QQ号168777108")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XiaoYunCN/VIP/main/%E4%BA%91%E8%84%9A%E6%9C%AC/UNIVERSAL%20VERSION.LUA", true))()

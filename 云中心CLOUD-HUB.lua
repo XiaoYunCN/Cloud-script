332b54c00718729ae2dbfdc18a46f52cafbc3bf6
小云 <130466576+XiaoYunCN@users.noreply.github.com>
2024-11-19 00:23:10 +0800

    更新 云中心CLOUD-HUB.lua

--- 云中心CLOUD-HUB.lua
+++ 云中心CLOUD-HUB.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com//XiaoYunCN/VIP/main/%E4%BA%91%E8%84%9A%E6%9C%AC%2FUNIVERSAL%20VERSION.LUA"))()
+game:GetService("Players").LocalPlayer:Kick("云脚本QQ群号296846962")
